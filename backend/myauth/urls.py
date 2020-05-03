5382ce8394dbc576e50e8faead28a7f66d6807a7
Ainur Makymetova <cmak@technodom.kz>
2020-05-03 08:22:47 +0600

    backend | auth

--- backend/myauth/urls.py
+++ backend/myauth/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
-    
+    path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup', views.signUp.as_view(), name="signup")
 ]
--- backend/myauth/views.py
+++ backend/myauth/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from .serializers import UserSerializer
+
+class signUp(CreateAPIView):
+    model = User
+    serializer_class = UserSerializer
