0e13798fab524a11cffa659e17ebe8e05abc17b8
moon1ightx <ainur.is1701@gmail.com>
2020-05-03 10:17:21 +0600

    get internships, vacancies, companies with filter

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('technology', views.TechViews.as_view()),
+    path('companies', views.CompanyViews.as_view()),
+    path('internships', views.IntershipViews.as_view()),
+    path('vacancies', views.VacancyViews.as_view()),
 ]
--- backend/items/views.py
+++ backend/items/views.py
@@ -16,3 +16,34 @@ class TechViews(APIView):
         techs = Techno.objects.all()
         serializer = self.serializer_class(techs, many=True)
         return Response(serializer.data)
+
+class CompanyViews(APIView):
+    serializer_class = CompanySerializer
+    def get(self, request, format=None):
+        companies = Company.objects.all()
+        title_contains_query = request.GET.get('name')
+        if title_contains_query!='' and title_contains_query is not None:
+            companies=companies.filter(name__icontains=title_contains_query)
+        serializer = self.serializer_class(companies, many=True)
+        return Response(serializer.data)
+
+class VacancyViews(APIView):
+    serializer_class = VacancySerializer
+    def get(self, request, format=None):
+        vacancies = Vacancy.objects.all().prefetch_related('techno')
+        title_contains_query = request.GET.get('title')
+        if title_contains_query!='' and title_contains_query is not None:
+            vacancies=vacancies.filter(title__icontains=title_contains_query).prefetch_related('techno')
+        serializer = self.serializer_class(vacancies, many=True)
+        return Response(serializer.data)
+
+class IntershipViews(APIView):
+    serializer_class = IntershipSerializer
+    def get(self, request, format=None):
+        internships = Internship.objects.all().prefetch_related('techno')
+        title_contains_query = request.GET.get('title')
+        if title_contains_query!='' and title_contains_query is not None:
+            internships=internships.filter(title__icontains=title_contains_query).prefetch_related('techno')
+        serializer = self.serializer_class(internships, many=True)
+        return Response(serializer.data)
+
