7015904670c0d1f012aa451451b35bc7b716956e
moon1ightx <ainur.is1701@gmail.com>
2020-05-08 19:35:51 +0600

    ui test

--- JobifyMobile/app/src/main/java/kz/iitu/jobifymobile/views/MainActivity.kt
+++ JobifyMobile/app/src/main/java/kz/iitu/jobifymobile/views/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import kz.iitu.jobifymobile.R
 import kz.iitu.jobifymobile.data.models.Vacancy
 import kz.iitu.jobifymobile.data.networking.ApiFactory
@@ -61,6 +63,32 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.loadStories()
         mainViewModel.loadInterships()
         mainViewModel.loadStacks()
+        bottom_navigation.selectedItemId = R.id.page_1
+
+        if(bottom_navigation.selectedItemId == R.id.page_2) {
+            txt.text = "Page 2"
+        }
+        if(bottom_navigation.selectedItemId == R.id.page_1) {
+            txt.text = "Page 1"
+        }
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+
+                    true
+                }
+                R.id.page_2 -> {
+
+                    true
+                }
+                R.id.page_3 -> {
+
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun setupVacancies(vacancies: List<Vacancy>){
