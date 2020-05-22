ba857e9ce0ff7a1dca0b6dc3fc2510e635497788
moon1ightx <ainur.is1701@gmail.com>
2020-05-22 17:27:48 +0600

    store

--- frontend/src/containers/explore/index.js
+++ frontend/src/containers/explore/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {  Link } from "react-router-dom";
 import Header from '../../components/header'
 import Footer  from '../../components/footer'
+
 function Explore() {
   return (
     <div className="Explore">
